03a06dc0a1ad7a062be91999889f279b64291918
venkatej <venkatej.golamari@gmail.com>
2023-04-21 17:34:06 -0500

    Create variable.tf

---
+++ terraform/variable.tf
@@ -0,0 +1,8 @@
+variable resource_group_name {
+    type = string
+    description = "this is for test"
+}
+variable resource_group_location {
+    type = string
+    description = "this is location"
+}
