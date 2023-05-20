816100eed863c0d2b0961884770f9fe9ccebc0cb
venkatej <venkatej.golamari@gmail.com>
2023-05-20 09:45:29 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,10 @@
-provider "azurerm" {}
+provider "azurerm" {
 client_id = "zxcxvcbvnb"
 client_cd = "dfxfcfbvnmbj,nm./,fdfgdfghfhgj"
 tenant_id = "czfdgfhgjh"
-subscription_id = "efesfdgfthfhg
+subscription_id = "efesfdgfthfhg"
+features {}
+}
 
 resource "azurerm_resource_group" "rg" {
     name = "venkat"
